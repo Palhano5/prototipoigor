999f04721a337ac356f3982007b2fdd82a0eefb2
Palhano5 <igorpalhano2014barbosa@gmail.com>
2024-06-13 17:45:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Função para começar o jogo
+function startGame() {
+    window.location.href = "tela_de_jogo.html";
+}
+
+// Função para abrir as configurações
+function openSettings() {
+    window.location.href = "tela_de_configuracoes.html";
+}
+
+// Função para voltar à página anterior
+function goBack() {
+    window.history.back();
+}
